6eba8e4aae160e124f968fcffe27eed285c8d4f6
diegogglez <diegomiller7@gmail.com>
2022-10-12 13:39:06 +0200

    Iteración #1: Interacción con el DOM

---
+++ upgrade-dom/01-interaccion-con-dom/01-interacion-con-dom.js
@@ -0,0 +1,31 @@
+// 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+
+const buttom$$ = document.querySelector('.showme');
+console.log(buttom$$);
+
+// 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+
+const title$$ = document.querySelector('#pillado');
+console.log(title$$);
+
+// 1.3 Usa querySelector para mostrar por consola todos los p
+
+const arr$$ = document.querySelectorAll('p');
+console.log(arr$$[0].outerText);
+
+// 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+
+const pokemon$$ = document.querySelectorAll('.pokemon');
+console.log(pokemon$$);
+
+// 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
+// data-function="testMe".
+
+console.log(document.querySelectorAll('[data-function="testMe"]'));
+
+// 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
+// data-function="testMe".
+
+const characters$$ = document.querySelectorAll('[data-function="testMe"]');
+console.log(characters$$[2]);
+
